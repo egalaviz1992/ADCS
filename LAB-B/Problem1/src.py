6eff7c4319e8e35d8f2f7883bd91537763fc47d0
Alfonso <alfonso.colung@gmail.com>
2018-02-08 11:10:29 -0600

    LAB-B, Problems1,2
    Add comments to src files in probs1,2

--- LAB-B/Problem1/src.py
+++ LAB-B/Problem1/src.py
@@ -1,4 +1,21 @@
 def isMultiple_h(num, check, doPrint, oddEven):
+	"""
+		Description:
+			Checks if num is multiple of check.
+			The output depends on variables doPrint and oddEven.
+			doPrint defines whether the output will be printed or if a boolean
+			test is being issued
+			oddEven defines whether we're doing an odd/even check or if we're
+			doing a -divides evenly- check
+		Input
+			num(number) number to evaluate
+			check(number) number to divide by
+			doPrint(boolean) print result?
+			oddEven(boolean) oddEven test?
+		Output
+			True/False if !doPrint
+			relationship among numbers if doPrint
+	"""
 	if (num % check == 0):
 		if doPrint == True:
 			if oddEven == True:
@@ -20,12 +37,37 @@ def isMultiple_h(num, check, doPrint, oddEven):
 			return False
 		
 def isMultiple(num, check):
+	"""
+		Description:
+			Checks if num is multiple of check
+		Input
+			num(number) number to evaluate
+			check(number) number to divide by
+		Output
+			divides evenly/does not divide evenly
+	"""
 	return isMultiple_h(num, check, True, False)
 
 def isOddOrEven(num):
+	"""
+		Description:
+			Checks if num is odd, even, or divisible by 4 (implicitly even)
+		Input
+			num(number) number to evaluate
+		Output
+			odd/even/divisible by 4
+	"""
 	return isMultiple_h(num, 2, True, True)
 	
 def isPrime(num):
+	"""
+		Description:
+			Checks is num is a prime number
+		Input
+			num(number) number to evaluate
+		Output
+			prime number/not a prime number
+	"""
 	for n in range(2, num):
 		if isMultiple_h(num, n, False, False):
 			return 'not a prime number'
--- LAB-B/Problem2/src.py
+++ LAB-B/Problem2/src.py
@@ -1,6 +1,22 @@
 from random import randint
 
 def commonMembers_h(a, b, acc):
+	"""
+		Description
+			Helper recursive function to find common members between to lists.
+			Base case:
+				either a or b are empty, return the accumulated list
+			Recursive case:
+				compare a[0] and b[0], if they are
+				-equal: add a[0] to acc and call recursively on the rest of both lists
+				-different (a[0] < b[0]): call recursively on the rest of a and same b
+				-different (a[0] > b[0]): call recursively on the rest of b and same a
+		Input
+			a,b sorted lists
+			acc accumulated list, should be [] for external calls
+		Output
+			list of common members in a and b
+	"""
 	if not a or not b:
 		return acc
 	if a[0] == b[0]:
@@ -15,14 +31,35 @@ def commonMembers_h(a, b, acc):
 		return commonMembers_h(a, b[1:], acc)
 		
 def commonMembers(a,b):
+	"""
+		Description
+			Find common members, without repetition, among list a and list b
+			We will sort both lists and call commonMembers_h
+		Input
+			a,b lists
+		Output
+			List of common members among a and b
+	"""
 	a.sort()
 	b.sort()
 	return commonMembers_h(a, b, [])
 	
 def commonMembersOneLiner(a,b):
+	"""
+		Description
+			Find common members, among list a and list b, using a one liner
+	"""
 	return list(set(a).intersection(b))
 	
 def createLists(listA,listB,listCommon):
+	"""
+		Description
+			Function to create two lists and the list of common members among them.
+			This is to be used to test the commonMembers function.
+			We will first add some numbers to the three lists (a,b,common) and then 
+			different numbers to lists a and b. Finally, we will sort all the lists and
+			remove repeated elements in the common list.
+	"""
 	for n in range(randint(5,10)):
 		num = randint(0,10)
 		listA.append(num)
